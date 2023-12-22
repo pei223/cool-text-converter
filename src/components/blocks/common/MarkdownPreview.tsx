7634f5fec8217e2e46c7e6d66c193a6cb2015645
pei <peidparc@gmail.com>
2023-12-22 18:43:37 +0900

    プレビュー機能追加

---
+++ src/components/blocks/common/MarkdownPreview.tsx
@@ -0,0 +1,44 @@
+import { Box, SxProps, Typography } from "@mui/material";
+import markdownit from "markdown-it";
+import styled from "styled-components";
+
+const md = markdownit({
+  html: true,
+});
+
+type Props = {
+  markdownText: string;
+  sx?: SxProps;
+};
+
+const StyledPre = styled.pre`
+  table {
+    min-width: 200px;
+  }
+  table,
+  th,
+  td {
+    border: 1px solid gainsboro;
+    border-collapse: collapse;
+  }
+
+  th,
+  td {
+    padding: 6px 12px;
+  }
+`;
+
+function MarkdownPreview({ markdownText, sx }: Props) {
+  return (
+    <Box sx={sx}>
+      <Typography color="text.secondary" variant="h5">
+        Preview
+      </Typography>
+      <StyledPre
+        dangerouslySetInnerHTML={{ __html: md.render(markdownText) }}
+      />
+    </Box>
+  );
+}
+
+export default MarkdownPreview;
